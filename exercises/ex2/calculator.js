c36ae2d814f6bc248179d852d0b452edf4341998
talesaraujo <talesaraujodossantos@gmail.com>
2019-04-21 20:16:47 -0300

    Exercise 2

---
+++ exercises/ex2/calculator.js
@@ -0,0 +1,18 @@
+var sum = (x, y) => x + y;
+
+var subtract = (x, y) => x - y;
+
+var multiply = (x, y) => x * y;
+
+var divide = (x, y) => (y === 0) ? undefined : x / y;
+
+function pow(x, y) {
+    var p = 1;
+
+    for (var i = 0; i < y; i++) {
+        p = p * x;
+    }
+    return p;
+}
+
+
